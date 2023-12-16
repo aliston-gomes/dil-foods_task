0056be9564deccc28a38af5fc5dbd30dd771ad26
Aliston <aliston4@icloud.com>
2023-12-17 05:01:47 +0530

    created product card component

---
+++ e-commerce-app/src/components/ProductCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <div className="bg-red-400 w-48 md:w-60 lg:w-60 h-52 md:h-72 border rounded-lg">
+        <img src="" alt="" />
+        <div>title</div>
+        <div>Rating</div>
+        <div>Price</div>
+        <div>Wishlist</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
