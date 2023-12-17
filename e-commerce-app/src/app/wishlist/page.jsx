2429be9f5579b620c5a441cd06a372974e28a967
Aliston <aliston4@icloud.com>
2023-12-17 23:49:40 +0530

    responsivenss for different screen sizes added

--- e-commerce-app/src/app/wishlist/page.jsx
+++ e-commerce-app/src/app/wishlist/page.jsx
@@ -5,7 +5,7 @@ const wishList = () => {
   const [products, setProducts] = useState([, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   return (
     <>
-      <section className="grid grid-cols-1 gap-6 p-2">
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-2">
         {products.map((item, index) => {
           return (
             <div className="bg-slate-500 h-48" key={index}>
