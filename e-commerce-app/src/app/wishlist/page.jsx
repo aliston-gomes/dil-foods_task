c0088395ed7a2bb9430ae37f52f5912ed5d49746
Aliston <aliston4@icloud.com>
2023-12-17 23:42:33 +0530

    cart page/ui created

---
+++ e-commerce-app/src/app/wishlist/page.jsx
@@ -0,0 +1,21 @@
+"use client";
+import React, { useState } from "react";
+import { BsBagHeartFill } from "react-icons/bs";
+const wishList = () => {
+  const [products, setProducts] = useState([, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  return (
+    <>
+      <section className="grid grid-cols-1 gap-6 p-2">
+        {products.map((item, index) => {
+          return (
+            <div className="bg-slate-500 h-48" key={index}>
+              {item}
+            </div>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+export default wishList;
