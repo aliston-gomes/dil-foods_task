ed874ed8eccb50d79e88d693b1b45109cb688704
Aliston <aliston4@icloud.com>
2023-12-17 23:42:15 +0530

    cart,  wishlist navigation buttons added to navbar

--- e-commerce-app/src/app/navBar/NavBar.jsx
+++ e-commerce-app/src/app/navBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import { BsBagHeartFill,BsFillCartFill } from "react-icons/bs";
 
 const NavBar = () => {
   return (
@@ -7,7 +8,15 @@ const NavBar = () => {
       <section className="h-14 md:h-20 lg:h-20 bg-[#E64848] flex items-center justify-center w-full ">
         <article className="flex items-center justify-between w-[90%]  h-14 sm:h-32 md:h-20 lg:h-20">
           <Link href="/">Brand Logo</Link>
-          <Link href="productDetails/2">Sign In</Link>
+          <div className="flex md:gap-4 gap-6 lg:gap-10">
+            <Link href="/wishlist">
+            <BsBagHeartFill className="md:text-2xl lg:text-2xl text-xl "/>
+            </Link>
+            <Link href="/wishlist">
+            <BsFillCartFill className="md:text-2xl lg:text-2xl text-xl "/>
+            </Link>
+            </div>
+          
         </article>
       </section>
     </div>
