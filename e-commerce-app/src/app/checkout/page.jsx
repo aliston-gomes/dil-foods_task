610f07294eb09fffa0147f004a379bff4f990972
Aliston <aliston4@icloud.com>
2023-12-19 15:11:12 +0530

    simple mockup for ordered placed

---
+++ e-commerce-app/src/app/checkout/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+const checkOut = ({ params }) => {
+  return (
+    <section className="h-[100vh] bg-[#5EAAA8] text-2xl font-extrabold flex items-center justify-center w-full">
+      <h1 className="flex items-center justify-center text-white w-96">
+        Order Placed Successfully!
+      </h1>
+    </section>
+  );
+};
+export default checkOut;
