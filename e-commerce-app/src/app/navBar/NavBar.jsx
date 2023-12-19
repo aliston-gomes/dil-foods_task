aff0f644b44a82306df49b01b8d52c9ab5b59d34
Aliston <aliston4@icloud.com>
2023-12-19 15:12:49 +0530

    nav bar icons added

--- e-commerce-app/src/app/navBar/NavBar.jsx
+++ e-commerce-app/src/app/navBar/NavBar.jsx
@@ -7,16 +7,17 @@ const NavBar = () => {
     <div>
       <section className="h-14 md:h-16 lg:h-16 bg-[#E64848] flex items-center justify-center w-full ">
         <article className="flex items-center justify-between w-[90%]  h-14 sm:h-32 md:h-20 lg:h-20">
-          <Link href="/">Brand Logo</Link>
+          <Link href="/" className="text-lg font-bold text-white">
+            E-Com
+          </Link>
           <div className="flex md:gap-4 gap-6 lg:gap-10">
             <Link href="/wishlist">
-            <BsBagHeartFill className="md:text-2xl lg:text-2xl text-xl "/>
+              <BsBagHeartFill className="md:text-2xl lg:text-2xl text-xl text-white" />
             </Link>
             <Link href="/cart">
-            <BsFillCartFill className="md:text-2xl lg:text-2xl text-xl "/>
+              <BsFillCartFill className="md:text-2xl lg:text-2xl text-xl text-white " />
             </Link>
-            </div>
-          
+          </div>
         </article>
       </section>
     </div>
